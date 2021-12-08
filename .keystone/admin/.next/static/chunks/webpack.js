a8f3688efd666a161bef8f0702a760e5c9a0fc68
Alex <kuzminbass@gmail.com>
2021-12-08 11:37:17 +1000

    npm update

--- .keystone/admin/.next/static/chunks/webpack.js
+++ .keystone/admin/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5e10f07d43837872e4e3";
+/******/ 	var hotCurrentHash = "dbd44ecefebed8b15afb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
