99ff274a6a8ccd29c164eedd095292d103e2fb36
Alex <kuzminbass@gmail.com>
2021-12-10 09:21:17 +1000

    to main head

--- .keystone/admin/.next/static/chunks/webpack.js
+++ .keystone/admin/.next/static/chunks/webpack.js
@@ -113,11 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-<<<<<<< HEAD
-/******/ 	var hotCurrentHash = "83c01aa80657ab17a55b";
-=======
-/******/ 	var hotCurrentHash = "fbd2f3b23d2cf7e02287";
->>>>>>> parent of 35b02f6 (more seed data)
+/******/ 	var hotCurrentHash = "963b4daab30feb547e20";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
